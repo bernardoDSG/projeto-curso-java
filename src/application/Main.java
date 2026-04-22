e05de39d4e6344040b721febf86df763e852eb5c
bernardoDSG <bernardosantos@unitins.br>
2026-04-22 16:48:36 -0300

    working with archives in java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,37 +1,47 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Locale;
-import java.util.Scanner;
 
-import model.entities.Account;
-import model.exceptions.DomainException;
+import model.entities.Product;
 
 public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
-        try {
-            System.out.println("Enter account data");
-            System.out.print("Number: ");
-            int number = sc.nextInt();
-            System.out.print("Holder: ");
-            sc.nextLine();
-            String holder = sc.nextLine();
-            System.out.print("Initial balance: ");
-            double initialBalance = sc.nextDouble();
-            System.out.print("Withdraw limit: ");
-            double withdrawLimit = sc.nextDouble();
-            Account account = new Account(number, holder, initialBalance, withdrawLimit);
-            System.out.println();
-            System.out.print("Enter amount for withdraw: ");
-            double amount = sc.nextDouble();
-            account.withdraw(amount);
-            System.out.printf("New balance: %.2f", account.getBalance());
-        } catch (DomainException domEx) {
-            System.out.println(domEx.getMessage());
-        } finally {
-            sc.close();
+        String pathI = "C:\\Users\\12068496739\\Documents\\ioJava.csv";
+        Product[] products = new Product[4];
+        try (BufferedReader br = new BufferedReader(new FileReader(pathI))) {
+            String line = br.readLine();
+            int i = 0;
+            while (line != null && i < 4) {
+                String[] data = line.split(",");
+                products[i] = new Product(data[0], Double.parseDouble(data[1]), Integer.parseInt(data[2]));
+                i++;
+                line = br.readLine();
+            }
+
+        } 
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
         }
+        
+        
+            System.out.println("Directory created successfully!");
+            String pathO = pathI + "\\out\\summary.csv";
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(pathO))) {
+                for (Product product : products) {
+                    bw.write(product.toString());
+                    bw.newLine();
+                }
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+          
 
     }
+
 }
